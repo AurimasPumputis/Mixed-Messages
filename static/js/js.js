857d6ec400d2dd0be73dbd80e75127195476b128
Aurimas <aurimassp@gmail.com>
2021-02-07 00:07:39 +0200

    Testing an API to get first part of the message

---
+++ static/js/js.js
@@ -0,0 +1,14 @@
+const request = new XMLHttpRequest();
+const randDay = Math.floor(Math.random() * 3000);
+const requestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randDay}&api_key=fdQxe7OGPA0uiJdQ1f8upc2o1YOsvZYzKuhFBL0t`;
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = () => {
+    const response = request.response;
+    const obj = response['photos'];
+    const randPhoto = obj[Math.floor(Math.random() * Object.keys(obj).length)];
+    console.log(randPhoto['img_src']);
+
+}
