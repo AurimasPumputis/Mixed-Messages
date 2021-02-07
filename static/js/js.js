a576318d1daaa4782936d74c4d8da4bd891a7f8f
Aurimas <aurimassp@gmail.com>
2021-02-07 15:12:45 +0200

    Refactored photo API into a function

--- static/js/js.js
+++ static/js/js.js
@@ -1,14 +1,16 @@
-const request = new XMLHttpRequest();
-const randDay = Math.floor(Math.random() * 3000);
-const requestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randDay}&api_key=fdQxe7OGPA0uiJdQ1f8upc2o1YOsvZYzKuhFBL0t`;
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
-request.onload = () => {
-    const response = request.response;
-    const obj = response['photos'];
-    const randPhoto = obj[Math.floor(Math.random() * Object.keys(obj).length)];
-    console.log(randPhoto['img_src']);
-
+//Returns a random photo URL from NASA Mars Rover Photos API
+const marsPhoto = () => {
+    const request = new XMLHttpRequest();
+    const randDay = Math.floor(Math.random() * 3000);
+    const requestURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randDay}&api_key=fdQxe7OGPA0uiJdQ1f8upc2o1YOsvZYzKuhFBL0t`;
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = () => {
+        const response = request.response;
+        const obj = response['photos'];
+        const randPhoto = obj[Math.floor(Math.random() * Object.keys(obj).length)];
+        console.log(randPhoto['img_src']);
+    }
 }
+marsPhoto();
